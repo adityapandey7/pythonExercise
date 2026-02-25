aae87f7daa904c8d8845a2aabd3ced2c781e6357
Aditya Pandey <adityapandey.kv@gmail.com>
2026-02-25 16:04:57 +0530

    tuple ex1

---
+++ tuple/ex1.py
@@ -0,0 +1,13 @@
+# Repeat a below tuple three times.
+
+# Given:
+
+# original_tuple = ('a', 'b')
+# Expected Output:
+
+# ('a', 'b', 'a', 'b', 'a', 'b')
+
+original_tuple = ('a', 'b')
+new_tuple = original_tuple * 3
+
+print(f"new tuple {new_tuple}")
